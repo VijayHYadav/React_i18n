669b5c375bc8d3581c03457ce2ef488c55d20264
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-24 04:50:36 +0530

    commit at 14.42Timeline.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { I18nProvider, LOCALES } from './i18n'
+import { FormattedMessage } from 'react-intl';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <I18nProvider locale={LOCALES.ENGLISH}>
+      <div className="App">
+        <header className="App-header">
+          <FormattedMessage id="hello"/>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </I18nProvider>
   );
 }
 
--- src/i18n/provider.js
+++ src/i18n/provider.js
@@ -4,8 +4,10 @@ import { IntlProvider } from "react-intl";
 import { LOCALES } from "./locales";
 import messages from "./messages";
 
-const Provider = ({children, locale}) => (
+const Provider = ({children, locale = LOCALES.ENGLISH}) => (
     <IntlProvider locale={locale} textComponent={Fragment} messages={messages[locale]}>
         {children}
     </IntlProvider>
-)
+);
+
+export default Provider
