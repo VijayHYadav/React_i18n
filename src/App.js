2a356b4c5297e141062ec86a0f07a6ea96de70a0
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-24 05:07:11 +0530

    commit at 22:1Timeline.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import { I18nProvider, LOCALES } from './i18n'
 import { FormattedMessage } from 'react-intl';
 
 import translate from './i18n/translate';
+import { useState } from 'react';
 
 function App() {
+
+  const [locale, setLocale] = useState(LOCALES.ENGLISH);
+
   return (
-    <I18nProvider locale={LOCALES.FRENCH}>
+    <I18nProvider locale={locale}>
       <div className="App">
         <header className="App-header">
           {translate("hello")}
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             {translate('edit', {path: <code>src/App.js</code>})}
-            {/* Edit <code>src/App.js</code> and save to reload. */}
+            <hr />
+            Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
@@ -25,6 +30,10 @@ function App() {
           >
             Learn React
           </a>
+          <hr />
+          <button onClick={() => setLocale(LOCALES.ENGLISH)}>ENGLISH</button>
+          <button onClick={() => setLocale(LOCALES.FRENCH)}>FRENCH</button>
+          <button onClick={() => setLocale(LOCALES.GERMAN)}>GERMAN</button>
         </header>
       </div>
     </I18nProvider>
