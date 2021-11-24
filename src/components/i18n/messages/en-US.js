902fc6866afbb7d82af23725b85d456fa302cfd5
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-24 05:48:18 +0530

    commit at 14:6Timestamp.

---
+++ src/components/i18n/messages/en-US.js
@@ -0,0 +1,9 @@
+import { LOCALES } from "../locales"
+
+export default {
+    [LOCALES.ENGLISH]: {
+        'hello': 'Hello message',
+        'edit': 'Edit {path} and save to reload.',
+        'report': 'Report'
+    }
+}
